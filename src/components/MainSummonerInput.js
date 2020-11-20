d1f9d21bc601c06670eaf06685dfabd40514b6ae
Miri Yu <zztio@ajou.ac.kr>
2020-11-21 02:43:07 +0900

    add api request, Summoner (temp)

--- src/components/MainSummonerInput.js
+++ src/components/MainSummonerInput.js
@@ -1,19 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 import './MainSummonerInput.css'
 
-function MainSummonerInput(){
+const MainSummonerInput = () => {
+  const [users, setUsers] =useState(null);
+  const [loading , setloading] =useState(false);
+  const [error, setError] =useState(null);
+  const apiCall = async (name) => {
+    
+    try{
+      setError(null);
+      setUsers(null);
+      setloading(true);
 
-    return(
-        <form>
-        <input 
-          type = "UserName" 
-          name = "UserName" 
-          placeholder = "Hide On Bush"
-          className="search"
-         />
-        <button className = "searchButton"/>
-      </form>
-    );
+        const response = await axios.get(
+          'http://61.99.75.232:5000/specpage/Hideonbush',{
+            // params: {
+            //   name:'hide on bush'
+            // }
+          }
+        );
+        console.log(response.data)
+        return response;
+    } catch(error){
+        setError(error);
+    }
+    setloading(false);
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+  }
+  const onSubmit = async(event) => {
+    event.preventDefault();
+    const summonername = apiCall()
+          .then(response => {
+          if (response){
+              console.log(Object.entries(summonername.data.message))
+            }
+          })
+          .catch(error => {
+          console.log(error);
+          })
+    console.log('call api')
+  }
+  if (loading) return <div className='loading'></div>
+  if (error) return <Link to="/ModelPage"></Link>
+  return(
+    <form onSubmit={onSubmit}>
+       <input 
+         type = "UserName" 
+         name = "UserName" 
+         placeholder = "Hide On Bush"
+         className="search"
+        />
+       <button className = "searchButton"/>
+     </form>
+   );
 
 }
 
--- src/components/Summoner.js
+++ src/components/Summoner.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Summoner.css'
+import ProfileIocn from '../img/TempProfileicon.png'
+import Tier from '../img/TempTier.png'
 
 function Summoner(props){
-
+    
     return(
-        <div>
-            
+        <div className="ProfileContainer">
+            <img src = {ProfileIocn} alt="Profileicon" className = "ProfileIcon"/> 
+            <b className = "SummonerName">Hide On Bush</b>
+            <button className = "UpdateButton">전적갱신</button>
+                
+            <img src = {Tier} alt="TierIcon" className = "TierIcon"/>
+                
+            <b className = "TierName">Master</b>
+            <br></br>
+            <b className = "LP">284LP</b>
+
         </div>
     );
 
