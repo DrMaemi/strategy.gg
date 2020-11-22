9d443cf7c5e851483ebbc87fd77852a612193e81
DrMaemi <leesh1510@ajou.ac.kr>
2020-11-22 11:53:49 +0900

    replace Typo

--- preprocessfordb.py
+++ preprocessfordb.py
@@ -2,6 +2,6 @@ letters = ["+", " ", "%20"]
 
 def processString(string):
     for letter in letters:
-        string = string.relace(letter, "")
+        string = string.replace(letter, "")
     string = string.lower()
     return string
